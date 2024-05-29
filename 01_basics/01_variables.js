bbe135aa377f7e227a2ecb947590f8c2baf84223
lowkey-develops <lowkey.develops@gmail.com>
2024-05-29 12:53:36 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "lowkey-develops"
+var accountPass= 1234
+let accountEmail="Archit@google.com"
+accountCity="Kolkata"
+
+/*accountId=12
+ Do not use var in your builds as it does not have block scope and functional scope. Use let instead.*/
+accountPass=4321
+accountEmail="logo@gmail.com"
+accountCity="Canada"
+
+console.table([ accountId, accountPass, accountEmail, accountCity])
