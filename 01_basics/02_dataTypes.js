72ee631ce1a255a96d8f646a50424a76145d39a2
lowkey-develops <lowkey.develops@gmail.com>
2024-05-29 13:24:35 +0000

    data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";// treats all js code as newer version
+//alert(10+1) we are using nodejs not browser.. this does not run in node. There is another way to do it.
+// code readability of the should be high always use best practices to become a good developer
+// To learn in real depth go to ECMA script standard documentation and mdn.
+// Datatypes
+/*
+number => 2 power of 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined 
+symbol => unique used in react and all
+object
+
+let r= "jasper"
+console.log(typeof null) type of null is object and undefined is undefined.
+*/
