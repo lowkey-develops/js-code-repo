446d9d9733b35bb8310a27101df4593eb82ed098
lowkey-develops <lowkey.develops@gmail.com>
2024-05-30 15:00:19 +0000

    Summary less theory

---
+++ 01_basics/dataTypes_Summary.js
@@ -0,0 +1,7 @@
+// Primitive and Non-primitive data types, categorised on the basis of how data is stored and accessed
+
+// Primitive (when called called by value, which means copy of the data is sent no the original data)
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Non-primitive (reference type)
+// Array, Objects, functions
