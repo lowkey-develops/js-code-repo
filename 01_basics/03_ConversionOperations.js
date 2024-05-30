50e5297263658d9169e3aa425d88ccd0c7522bdb
lowkey-develops <lowkey.develops@gmail.com>
2024-05-30 13:52:00 +0000

    Postfix prefix covered

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,52 @@
+let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+let valueInNumber= Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+let score2= null //null when converted to number gives 0. So be careful when youre using null with temp
+/*  To Number conversion
+    "33" => 33
+    "33abc" => NaN (Not a Number)
+    true => 1; flase => 0
+
+    To Boolean conversion
+    1 => true; 0 => flase
+    "" => false
+    "Archit" => true
+
+    To String conversion
+    Converts to string directly
+    */
+   // ***** Operations *****
+// let value= 3
+// let Negvalue= -value
+// console.log(Negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // to the power of
+// console.log(2/2)
+// console.log(2%10)
+let str1= "hello"
+let str2= " Archit"
+let str3= str1+str2
+// console.log(str3)
+// console.log( "1" + 2 )
+// console.log( 1 + "2" )
+// console.log( 1 + 1 + "2" )
+// console.log( "1" + 1 + 1 )
+// console.log(+true) //converts boolean to number due to + operator. Returns 1. + after true !allowed.
+// console.log(+"") // double quotes gives false which is converted to number so returns 0
+
+// let num1= 100
+// console.log(num1++)
+// console.log(++num1) prefix postfix increment decrement operator
+
+
+
+
+
+
