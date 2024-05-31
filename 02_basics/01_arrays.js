74c05b7d52fdff8308df41f5d855c4ce5649be51
lowkey-develops <lowkey.develops@gmail.com>
2024-05-31 12:57:21 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+//array
+const myArr=[1, 2, 3, 4, 5, 6]
+const myheros=["shaktimaan", "naagraj"]
+const myArr2= new Array(1,2,3,4,5)
+// console.log(myArr2[3])
+
+// Array Methods
+
+myArr.push(3)
+myArr.push(8)
+myArr.pop()
+console.log(myArr)
+myArr.unshift(3)
+console.log(myArr)
+myArr.shift(1)
+console.log(myArr)
