218a3eae19168f351f508e4e795fac85b5146d0a
lowkey-develops <lowkey.develops@gmail.com>
2024-05-31 11:49:00 +0000

    Random Function Covered

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score=400
+// console.log(score)
+// const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))  // returns 100.0 for 1 and 100.00 for 2
+
+// const otherNumber=123.4555
+// console.log(otherNumber.toPrecision(4))
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString())// returns in US standard that is millions  and hundred thousand
+// console.log(hundreds.toLocaleString('en-IN')) // returns in indian standard in lakhs and so on.
+
+//+++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(3.5))
+// console.log(Math.ceil(3.1))
+// console.log(Math.floor(3.9))
+// console.log(Math.min(3,5,7,1))
+// console.log(Math.max(3,1,3,4,5,9))
+
+console.log(Math.floor((Math.random()*10)+1))
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * ((max - min) + 1) + min)) // Random Number between a given range.
+
+
