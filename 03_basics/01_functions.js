45179c892c8ba145a09e062786d2c6039f06d7a6
lowkey-develops <lowkey.develops@gmail.com>
2024-06-02 17:17:52 +0000

    functions stated

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,44 @@
+
+
+function saymyName(){
+    console.log("a")
+    console.log("r")
+    console.log("c")
+    console.log("h")
+    console.log("i")
+    console.log("t")
+}
+
+//saymyName // this is giving reference to the function and saymyName() i.e with parenthesis is executing the function.
+//saymyName()
+// function addTwoNumbers(num1, num2){
+//     console.log(num1+num2)
+// }
+
+function addTwoNumbers(num1, num2){
+    // let  result=num1+num2
+    // return result // return should always be the last line in the function as no code after the return statement is executed.
+    //console.log("archit")// wont be executed but if written above return statement it will execute.
+    return num1+num2
+}
+
+// addTwoNumbers(3,4)// returns 7 when both are number data type
+// addTwoNumbers(3,"4")// returns 34 as it thinks if 4 is a string 3 must be a string this is javascript
+// addTwoNumbers(3, null)// onlu returns 3
+
+// when you pass some values while calling functions they are called arguments eg. addTwoNumbers(3,4) and when you create a function
+// and add values to accept they are called parameters.
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ",result) // there is no value in result as the function did not return any value so result is undefined 
+
+function loginUserMessage(username)// if you want a default value just write it, eg. (username="Arun"), and if an argument is passed it will overide
+{
+    if(username===undefined) // this can also be written as (!username)
+        {
+        console.log(`Please enter a username`)
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage())
+console.log(loginUserMessage("archit"))
